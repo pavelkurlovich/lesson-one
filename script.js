c999d3e6d5664b5bd15dee45acd1abff68fb12b7
pavelkurlovich <61707456+pavelkurlovich@users.noreply.github.com>
2020-11-18 17:31:20 +0300

    first commit(only numbers)

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+let num = (3);
+alert (num);
+
+let a=10;
+let b=2;
+alert(a+b);
+alert(a-b);
+alert(a*b);
+alert(a%b);
+
+let c=15;
+let d=2;
+let result = c + d;
+alert (result);
+
+function sum() {
+    let a=10;
+    let b=2;
+    let c=5;
+    return a+b+c;
+}
+alert (sum());
+
+function result2() {
+    let a=17;
+    let b=10;
+    let c=(a-b);
+    let d=7;
+    return c+d;
+}
+alert (result2());
+
